3487ecfe494fe9a8fe26d5a92d39e1bb9782d9db
ayushsinghh0 <153765126+ayushsinghh0@users.noreply.github.com>
2025-09-28 19:38:00 +0000

    day 2

--- Basics/01_test.js
+++ Basics/01_test.js
@@ -1,1 +1,3 @@
-console.log("ayush ")
+console.log("ayush");
+console.log("hello world");
+console.log("hello ayush");
---
+++ Basics/variables.js
@@ -0,0 +1,7 @@
+const accountId = 123456
+let accountemail = "ayushraj4820@gmail.com"
+var accountPassword = "myPassword@123"
+account=1 
+console.log(accountId)
+console.log(accountemail)
+console.table([accountId,accountemail,accountPassword,account])
